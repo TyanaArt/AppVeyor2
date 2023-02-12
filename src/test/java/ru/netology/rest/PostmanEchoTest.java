b3b3dd456498eb4a0844ecd03ccabe47d9647e28
turchaninova56 <tata_arta@vk.com>
2023-02-12 09:57:23 +0300

    modified test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -3,7 +3,6 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 
 class PostmanEchoTest {
@@ -12,12 +11,11 @@ class PostmanEchoTest {
     void shouldReturnSenData() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Data2")
+                .body("Da5ta2")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("Data2"));
-                //.body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
     }
 }
